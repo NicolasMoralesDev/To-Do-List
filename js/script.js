300cf60a0537b74e28c62747abd80fc8db114257
Nicolas <nicolasmoralesj03@gmail.com>
2022-04-27 11:20:42 -0300

    se mejora la estetica del form

--- js/script.js
+++ js/script.js
@@ -10,13 +10,22 @@ const li = document.getElementById('li');
   /* Crear tareas */
 
 let crearUsuario = () =>{
+  if(usuarioEntrada.value == '' || documentoEntrada.value == ''){
+   console.group(); 
+   console.debug('error');
+   alert('Rellena el campo...');
+   usuarioEntrada.focus();
+   documentoEntrada.focus();
+   console.groupEnd();
+  } else {
   let usuario = usuarioEntrada.value;
   let documento = documentoEntrada.value;
   let itemNueva = document.createElement('li');
   itemNueva.innerHTML=usuario; 
   list.append(itemNueva);
   itemNueva.append(documento);
-
+  
+  }
 };
 
 
