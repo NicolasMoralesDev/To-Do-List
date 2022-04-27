91df9422d666137f2f081022ac4d0718fecf9a0c
Nicolas <nicolasmoralesj03@gmail.com>
2022-04-27 13:53:49 -0300

    se mejoran la funcionalidad de agregado

--- js/script.js
+++ js/script.js
@@ -1,37 +1,52 @@
-
 /* Llamar ids */
+let listado = [];
+const usuarioEntrada = document.getElementById("usuario");
+const documentoEntrada = document.getElementById("documento");
+const list = document.getElementById("listado");
+const li = document.getElementById("li");
 
-const usuarioEntrada = document.getElementById('usuario');
-const documentoEntrada = document.getElementById('documento');
-const list = document.getElementById('listado');
-const li = document.getElementById('li');
+/* Crear tareas */
 
-
-  /* Crear tareas */
-
-let crearUsuario = () =>{
-  if(usuarioEntrada.value == '' || documentoEntrada.value == ''){
-   console.group(); 
-   console.debug('error');
-   alert('Rellena el campo...');
-   usuarioEntrada.focus();
-   documentoEntrada.focus();
-   console.groupEnd();
+let crearUsuario = () => {
+  if (usuarioEntrada.value == "" || documentoEntrada.value == "") {
+   
+    console.group();
+    console.debug("error");
+    alert("Rellena el campo...");
+    usuarioEntrada.focus();
+    documentoEntrada.focus();
+    console.groupEnd();
+    
   } else {
-  let usuario = usuarioEntrada.value;
-  let documento = documentoEntrada.value;
-  let itemNueva = document.createElement('li');
-  itemNueva.innerHTML=usuario; 
-  list.append(itemNueva);
-  itemNueva.append(documento);
-  
+    let datosLocal = {
+     nombre: usuarioEntrada.value,
+     documento: documentoEntrada.value
+    };
+    listado.push(datosLocal);
+    localStorage.setItem('item', JSON.stringify(listado));
+    console.group();
+    console.debug(localStorage.getItem('item'));
+    console.groupEnd();
+    mostrar();
   }
 };
 
+function mostrar () {
+
+    let valorLocal = localStorage.getItem('item');
+    valorLocal.forEach((intem, index ) => {
+      let usuario = usuarioEntrada.value;
+      let documento = documentoEntrada.value;
+      let itemNueva = document.createElement("li");
+      itemNueva.setAttribute("id", usuario);
+      list.append(itemNueva);
+      itemNueva.append(usuario);
+    });
+ 
+};
 
 /* Funcion para eliminar la tarea */
 
-OcultarLi = (e) =>{
-  e.target.parentElement.style.display='none';
+OcultarLi = (e) => {
+  e.target.parentElement.style.display = "none";
 };
-
